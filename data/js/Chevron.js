6ed83c7830e69fd01a7e15b71ccda2bc18d60ba0
leewayeserv <manishrustagi@leewayeserv.com>
2015-08-17 16:00:25 +0530

    Footer changed and Ourservices updated

---
+++ data/js/Chevron.js
@@ -0,0 +1,10 @@
+﻿function toggleChevron(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find("i.indicator")
+        .toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
+}
+$('#accordion2').on('hidden.bs.collapse', toggleChevron);
+$('#accordion2').on('shown.bs.collapse', toggleChevron);
+$('#accordion3').on('hidden.bs.collapse', toggleChevron);
+$('#accordion3').on('shown.bs.collapse', toggleChevron);
